d285efd92b79603fe46aa44d1394f477ff84d6b0
Chris Muntean <chrismunt123@gmail.com>
2024-10-28 20:12:43 -0700

    Code cleanup

--- train-start.py
+++ train-start.py
@@ -2,7 +2,7 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolo11n-pose.pt")  # load a pretrained model (recommended for training)
+model = YOLO("yolo11n-pose.pt")  # load a pretrained model
 
 # Train the model
 results = model.train(data="data.yaml", epochs=100, imgsz=640)
