a8152704ee8fac53636c66b01b13650c679af0a9
Chris Muntean <chrismunt123@gmail.com>
2024-10-31 21:23:14 -0700

    Added output video on webcam-test

--- webcam-test.py
+++ webcam-test.py
@@ -1,4 +1,4 @@
 from ultralytics import YOLO
 
 # Load the model and run inference on the webcam
-YOLO('runs/pose/train/weights/best.pt').predict(source=0, show=True, save=True)
+YOLO('runs/pose/train/weights/best.pt').predict(source=0, show=True, save=True, project='runs/pose/output', name='tests')
